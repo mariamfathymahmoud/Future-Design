48486ef2dc52212b901386dd99c1c81e151d9774
mariamfathymahmoud <mariamatmahmoud@gmail.com>
2022-07-20 10:56:46 +0200

    first commit

---
+++ python 2.py
@@ -0,0 +1,18 @@
+def inspect(x):
+    if x == 0:
+        print(x, "is zero")
+    elif x > 0:
+        print(x, "is positive")
+    elif x < 0:
+        print(x, "is negative")
+    else:
+        print(x, "is unlike anything I've ever seen...")
+
+inspect(0)
+inspect(-15)
+
+
+
+
+
+
